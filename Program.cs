fd8e64d385414a7d40404cf1d07bfd96e30ac7b5
Vrabbers <nininininimocp@gmail.com>
2020-06-21 16:24:07 -0300

    fix poo

--- Program.cs
+++ Program.cs
@@ -1,43 +1,51 @@
 ﻿using System;
-using System.IO;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-using System.Runtime.Serialization.Formatters.Binary;
 using Lokad.ILPack;
-using Microsoft.Win32.SafeHandles;
 using static System.Reflection.Emit.OpCodes;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ILGenerationTest
 {
     class Program
     {
-        static void Main(string[] argg)
+        static void Main(string[] args)
         {
-            Console.Write("Please input BRAIN FUCK CODE!\nCODE:");
-            var code = Console.ReadLine();
-            var asm = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("Brainfuck.NET Assembly"), AssemblyBuilderAccess.Run);
+            string code;
+            if (args.Length == 0)
+            {
+                Console.Write("Please input BRAIN FUCK CODE!\nCODE:");
+                code = Console.ReadLine();
+            }
+            else code = File.ReadAllText(args[0]);
+            
+
+            var asm = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("Brainfuck.NET Assembly"),
+                AssemblyBuilderAccess.Run);
             var module = asm.DefineDynamicModule("Brainfuck.NET Module");
-            var type = module.DefineType("BrainfuckNETOutput", TypeAttributes.Class | TypeAttributes.Sealed, null, new[]{IBrainfuck.Type});
-            var start = type.DefineMethod(nameof(IBrainfuck.Do), MethodAttributes.Public | MethodAttributes.Virtual, null, null);
-            type.DefineMethodOverride(start, IBrainfuck.Type.GetMethod(nameof(IBrainfuck.Do)) ?? throw new ArgumentNullException());
+            var type = module.DefineType("BrainfuckNETOutput", TypeAttributes.Class | TypeAttributes.Sealed, null,
+                new[] {IBrainfuck.Type});
+            var start = type.DefineMethod(nameof(IBrainfuck.Do), MethodAttributes.Public | MethodAttributes.Virtual,
+                null, null);
+            type.DefineMethodOverride(start,
+                IBrainfuck.Type.GetMethod(nameof(IBrainfuck.Do)) ?? throw new ArgumentNullException());
             var il = start.GetILGenerator();
             var tape = il.DeclareLocal(typeof(byte[]));
             var tptr = il.DeclareLocal(typeof(int));
             var labelStack = new Stack<(Label, Label)>();
-            var write = typeof(Console).GetMethod(nameof(Console.Write), new[] { typeof(char) });
+            var write = typeof(Console).GetMethod(nameof(Console.Write), new[] {typeof(char)});
             var read = typeof(Console).GetMethod(nameof(Console.Read), Type.EmptyTypes);
-            
+
             il.Emit(Ldc_I4, ushort.MaxValue + 1);
-            il.Emit(Newarr, typeof(byte)); 
+            il.Emit(Newarr, typeof(byte));
             il.Emit(Stloc, tape); //init array
             il.Emit(Ldc_I4_0);
             il.Emit(Stloc, tptr); //init tptr
 
-            foreach(var c in code)
+            foreach (var c in code)
             {
-                switch(c)
+                switch (c)
                 {
                     case '<':
                         il.Emit(Ldloc, tptr);
@@ -56,7 +64,7 @@ namespace ILGenerationTest
                         il.Emit(Stloc, tptr);
                         break;
                     case '+':
-                        il.Emit(Ldloc,tape);
+                        il.Emit(Ldloc, tape);
                         il.Emit(Ldloc, tptr);
                         il.Emit(Ldelema, typeof(byte));
                         il.Emit(Dup);
@@ -67,7 +75,7 @@ namespace ILGenerationTest
                         il.Emit(Stind_I1);
                         break;
                     case '-':
-                        il.Emit(Ldloc,tape);
+                        il.Emit(Ldloc, tape);
                         il.Emit(Ldloc, tptr);
                         il.Emit(Ldelema, typeof(byte));
                         il.Emit(Dup);
@@ -106,31 +114,31 @@ namespace ILGenerationTest
                         il.MarkLabel(forward);
                         break;
                 }
-            }    
+            }
+
             il.Emit(Ret);
 
-            if(labelStack.Count != 0)
+            if (labelStack.Count != 0)
                 throw new Exception("Mismatched brackets!");
-            
+
             var t = type.CreateType();
             if (t is null)
                 throw new NullReferenceException();
-            
+
             var filename = $"{t.Name}.dll";
-            if(t.GetConstructor(Type.EmptyTypes).Invoke(new object[0]) is IBrainfuck instance)
+            if (t.GetConstructor(Type.EmptyTypes)?.Invoke(new object[0]) is IBrainfuck instance)
             {
                 new AssemblyGenerator().GenerateAssembly(t.Assembly, filename);
                 Console.WriteLine("Generated assembly at {0}", filename);
                 instance.Do();
             }
-            else throw new MissingMemberException("Pee");
-
-        }   
+            else throw new MissingMemberException("Something has gone terribly wrong.");
+        }
     }
 
     public interface IBrainfuck
     {
-        public static Type Type = typeof(IBrainfuck);
+        public static readonly Type Type = typeof(IBrainfuck);
         public void Do();
     }
 }
